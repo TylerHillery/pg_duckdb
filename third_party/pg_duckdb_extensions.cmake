eb7af6ede232f951c78192c79109c6d0be73b7b2
Tyler Hillery <tyhillery@gmail.com>
2026-03-25 09:41:25 -0500

    chore: upgrade to duckdb 1.5.1

--- third_party/pg_duckdb_extensions.cmake
+++ third_party/pg_duckdb_extensions.cmake
@@ -2,5 +2,5 @@ duckdb_extension_load(json)
 duckdb_extension_load(icu)
 duckdb_extension_load(httpfs
     GIT_URL https://github.com/duckdb/duckdb-httpfs
-    GIT_TAG 9c7d34977b10346d0b4cbbde5df807d1dab0b2bf
+    GIT_TAG 7e86e7a5e5a1f01f458361bebdfa9b0a9a73a619
 )
